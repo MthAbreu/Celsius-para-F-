d986512dda52e775f925bcccc07ce9cd666d1088
MthAbreu <matheus.abreu18@etec.sp.gov.br>
2020-06-22 19:46:33 -0300

    Exercicio 2 de looping

---
+++ src/celsius/para/f/CelsiusParaF.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package celsius.para.f;
+
+import java.util.Scanner;
+
+public class CelsiusParaF {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Scanner entrada = new Scanner (System.in);
+        
+        double i, result;
+        
+        i = 10;
+        
+        while (i <= 100)
+        {
+            result = (i*9/5)+32;
+            System.out.println (result);
+            i=i+10;
+            
+        }
+    }
+    
+}
